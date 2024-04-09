c859e1b638c315b41d58786c1912eab3ef76ba73
NijeshShtha <anishnijesh117@gmail.com>
2024-04-09 14:35:15 +0545

    hawa, just hawa

--- setting.js
+++ setting.js
@@ -1,7 +1,15 @@
 let Mode = "light";
 let body = document.querySelector("body");
-let change = document.querySelector("button");
-change.addEventListener("click",() =>{
+let change = document.querySelector("img");
+let img = document.getElementById("toggle-img");
+img.onclick = function(){
+    document.body.classList.toggle("light");
+    if(document.body.classList.contains("light")){
+        img.src = "d-l mode1.png";
+    }
+    else{
+        img.src = "d-l mode2.png";
+    }
     if(Mode==="light"){
         Mode = "dark";
         body.classList.add("dark");
@@ -14,10 +22,12 @@ change.addEventListener("click",() =>{
         body.classList.remove("dark");
         localStorage.setItem('mode', 'light');
     }
-    console.log(Mode);
+    // console.log(Mode);
+}
+change.addEventListener("click",() =>{
+        
 });
-window.onload = function()
-{
+window.onload = function(){
     var storedColor = localStorage.getItem('mode');
     if(storedColor == 'dark')
     {
