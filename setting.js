638dd5fdbd881bae0cce438f19bd56dd320d55a7
NijeshShtha <anishnijesh117@gmail.com>
2024-04-22 16:38:22 +0545

    just hawa

--- setting.js
+++ setting.js
@@ -2,7 +2,7 @@ let Mode = "light";
 let body = document.querySelector("body");
 let change = document.querySelector("img");
 let img = document.getElementById("toggle-img");
-img.onclick = function(){
+change.addEventListener("click",() =>{
     document.body.classList.toggle("light");
     if(document.body.classList.contains("light")){
         img.src = "d-l mode1.png";
@@ -21,16 +21,13 @@ img.onclick = function(){
         body.classList.add("light");
         body.classList.remove("dark");
         localStorage.setItem('mode', 'light');
-    }
-    // console.log(Mode);
-}
-change.addEventListener("click",() =>{
-        
+    }   
 });
 window.onload = function(){
     var storedColor = localStorage.getItem('mode');
     if(storedColor == 'dark')
     {
         document.body.classList.add('dark');
+        img.src = "d-l mode2.png";
     }
 }
